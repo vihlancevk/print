2d4f2a9e0cda31600dae8c30a0677d28d41cf578
vihlancevk <vihlancevk@gmail.com>
2022-03-10 21:28:25 +0300

    Added specifier recognition %

--- print.asm
+++ print.asm
@@ -3,7 +3,7 @@
 
 section .data
 
-Msg:        db "Hello, world!", 0x0a
+Msg:    db "Hello, %cworld!", 0x0a
 
 section .text
 
@@ -31,7 +31,44 @@ next_strlen:
 	
 stop_strlen:
 	
-        ret
+    ret
+
+;------------------------------------------------
+; Search function for the first occurrence
+; of a character in a string
+;
+; Entry:	- RSI - addr of the beginning of the string
+;           - RAX - the symbol to be found
+; Note:     - $ - 24h (ASCII code)
+;           - returns 0 if the character is not in the string
+; Exit:		RCX - address of the first occurrence of the character in the str
+; Destr:	RAX, RBX, RCX, RSI, R8, R9
+;------------------------------------------------
+
+strchr:
+
+    mov r8, rsi
+    mov r9, rax
+	call strlen
+	mov rbx, rcx
+    mov rsi, r8
+    mov rax, r9
+	mov rcx, 0x1
+
+next_chr:
+    cmp [rsi], al
+	je stop_chr
+		
+    inc rcx
+    inc rsi
+    cmp rcx, rbx
+    jl next_chr
+
+    mov rcx, 0x0 
+        
+stop_chr:
+        
+	ret      
 
 ;------------------------------------------------
 ; Print str on screen
@@ -43,12 +80,33 @@ stop_strlen:
 
 print:
 
+next_specifier:
+    push si
+    mov rax, "%"
+    call strchr
+    pop si
+    cmp rcx, 0x0
+    je no_specifier
+
+    push rcx
+    mov rax, 0x1
+    mov rdi, 1
+    mov rdx, rcx
+    dec rdx
+    syscall     ; registers rcx and r11 will be destroyed
+    pop rcx
+    add rsi, rcx
+    ; ToDo
+    add rsi, 1
+    jmp next_specifier
+    
+no_specifier:
     push si
     call strlen
-    mov rdx, rcx
     pop si
+    mov rdx, rcx
     mov rdi, 1
-    mov rax, 0x01
+    mov rax, 0x1
     syscall
 
     ret
@@ -56,6 +114,7 @@ print:
 global _start
 
 _start:
+
     mov rsi, Msg
     call print
             
